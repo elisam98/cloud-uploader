48377c9d3b3adcfcbdff3fc0dd6e46e5cb6361dd
elisam98 <cornbeefonrye@gmail.com>
2016-11-23 22:11:52 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var restify = require('restify');
+
+var server = restify.createServer();
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get('/hello/:name', (req, res, next)=> {
+	res.send('hello ' + req.params.name);
+	next();
+});
+
+server.put('/recordings', (req, res, send)=> {
+	console.log(req.body);
+	res.send(req.body);
+});
+
+server.listen(8000, function() {
+	console.log('%s listening at %s', server.name, server.url);
+});
