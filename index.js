a91e7885c7ce981726aec6651c454f40fde873a1
elisam98 <cornbeefonrye@hotmail.com>
2016-12-06 00:34:59 -0500

    Remove node modules from package

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var restify = require('restify');
-var Busboy = require('busboy');
+var fs = require("fs");
 
 var server = restify.createServer();
 server.use(restify.queryParser());
@@ -11,25 +11,9 @@ server.get('/hello/:name', (req, res, next)=> {
 });
 
 server.put('/upload/:filename', (req, res, send)=> {
-	var busboy = new Busboy({ headers: req.headers });
-	busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-		console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-		file.on('data', function(data) {
-			console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
-		});
-		file.on('end', function() {
-			console.log('File [' + fieldname + '] Finished');
-		});
-	});
-	busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
-		console.log('Field [' + fieldname + ']: value: ' + inspect(val));
-	});
-	busboy.on('finish', function() {
-		console.log('Done parsing form!');
-		res.send('done');
-		res.end();
-	});
-	req.pipe(busboy);
+	console.log(req.body);
+	var wstream = fs.createWriteStream(req.params.filename);
+	req.pipe(wstream);
 });
 
 server.listen(8001, function() {
