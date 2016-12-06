a3fb93d0a635f3c039d76bb53e8a9917979a9a2e
elisam98 <cornbeefonrye@hotmail.com>
2016-12-06 02:00:09 -0500

    Added and completed Google Cloud Storage Uploads

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 var restify = require('restify');
 var fs = require("fs");
+var storage = require('@google-cloud/storage');
+
+var gcs = storage({
+	projectId: 'mytickets-nyc-151706',
+	keyFilename: 'mytickets-nyc.json'
+});
+var bucket = gcs.bucket('mytickets-records');
+
 
 var server = restify.createServer();
 server.use(restify.queryParser());
@@ -11,8 +19,11 @@ server.get('/hello/:name', (req, res, next)=> {
 });
 
 server.put('/upload/:filename', (req, res, send)=> {
+//	console.log(req.body);
+	var remoteWriteStream = bucket.file(req.params.filename).createWriteStream();
+	req.pipe(remoteWriteStream);
 /*
-	console.log(req.body);
+// Local File Storage
 	var wstream = fs.createWriteStream(req.params.filename);
 	req.pipe(wstream);
 */
