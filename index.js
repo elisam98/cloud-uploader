4ce87737135ed729d5a3c34c36163ae0ed748d74
elisam98 <cornbeefonrye@gmail.com>
2016-12-05 23:02:43 -0500

    switched to busboy for uploads

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var restify = require('restify');
+var Busboy = require('busboy');
 
 var server = restify.createServer();
 server.use(restify.queryParser());
@@ -10,9 +11,25 @@ server.get('/hello/:name', (req, res, next)=> {
 });
 
 server.put('/upload/:filename', (req, res, send)=> {
-	console.log(req.body);
-	console.log(req.params.filename);
-	res.send(req.params.filename);
+	var busboy = new Busboy({ headers: req.headers });
+	busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+		console.log('File [' + fieldname + ']: filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
+		file.on('data', function(data) {
+			console.log('File [' + fieldname + '] got ' + data.length + ' bytes');
+		});
+		file.on('end', function() {
+			console.log('File [' + fieldname + '] Finished');
+		});
+	});
+	busboy.on('field', function(fieldname, val, fieldnameTruncated, valTruncated) {
+		console.log('Field [' + fieldname + ']: value: ' + inspect(val));
+	});
+	busboy.on('finish', function() {
+		console.log('Done parsing form!');
+		res.send('done');
+		res.end();
+	});
+	req.pipe(busboy);
 });
 
 server.listen(8001, function() {
