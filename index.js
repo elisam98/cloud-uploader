81c28bc0bfbc2ce66b6ed69bfc4de09ded2a3c4b
elisam98 <cornbeefonrye@gmail.com>
2016-12-06 00:59:40 -0500

    Added Azure uploading

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var restify = require('restify');
 var fs = require("fs");
+var azure = require('azure-storage');
 
 var server = restify.createServer();
 server.use(restify.queryParser());
@@ -11,9 +12,21 @@ server.get('/hello/:name', (req, res, next)=> {
 });
 
 server.put('/upload/:filename', (req, res, send)=> {
+	var blobService = azure.createBlobService('myticketsnyc', 'nG16soRh1j9+2tq1/5sufobLpgD1Ja3jupfcXw5HutWv+mLhWGZpUrmWJg/CCWivdTDvhk9ijChCPHmuSd5gMg==');
+	blobService.createContainerIfNotExists('recordings', {
+		publicAccessLevel: 'blob'
+	}, function(error, result, response) {
+		if (!error) {
+			// if result = true, container was created.
+			// if result = false, container already existed.
+		}
+	});
+	blobService.createBlockBlobFromStream('recordings', req.params.filename, req);
+/*
 	console.log(req.body);
 	var wstream = fs.createWriteStream(req.params.filename);
 	req.pipe(wstream);
+*/
 });
 
 server.listen(8001, function() {
