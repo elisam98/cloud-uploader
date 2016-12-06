bdfff20d1942223c431a4d56fe7a02de7a121e17
elisam98 <cornbeefonrye@hotmail.com>
2016-12-06 01:19:32 -0500

    remove azure cloud

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var restify = require('restify');
 var fs = require("fs");
-var azure = require('azure-storage');
 
 var server = restify.createServer();
 server.use(restify.queryParser());
@@ -12,16 +11,6 @@ server.get('/hello/:name', (req, res, next)=> {
 });
 
 server.put('/upload/:filename', (req, res, send)=> {
-	var blobService = azure.createBlobService('myticketsnyc', 'nG16soRh1j9+2tq1/5sufobLpgD1Ja3jupfcXw5HutWv+mLhWGZpUrmWJg/CCWivdTDvhk9ijChCPHmuSd5gMg==');
-	blobService.createContainerIfNotExists('recordings', {
-		publicAccessLevel: 'blob'
-	}, function(error, result, response) {
-		if (!error) {
-			// if result = true, container was created.
-			// if result = false, container already existed.
-		}
-	});
-	blobService.createBlockBlobFromStream('recordings', req.params.filename, req);
 /*
 	console.log(req.body);
 	var wstream = fs.createWriteStream(req.params.filename);
